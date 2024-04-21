6c4295fc105e763a54a2bf7ddeaeda654ebb9e6a
Mario <48070830+mjcarranza@users.noreply.github.com>
2024-04-20 19:05:53 -0600

    creacion de proceso estadisticas

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
 SOURCE1 = creador.c								# Archivo fuente creador
 SOURCE2 = cliente.c								# Archivo fuente cliente
+SOURCE3 = estadisticas.c						# Archivo fuente estadisticas
+
 TARGET1 = creador_exe							# Ejecutable del creador
-TARGET2 = cliente_exe							# Ejecutabla del cliente
+TARGET2 = cliente_exe							# Ejecutable del cliente
+TARGET3 = estadisticas_exe						# ejecutable del reconstructor
+
 CC = gcc										# Compilador a utilizar
 CFLAGS = -Wall -Wextra -std=c99					# Opciones de compilación
 
-all: $(TARGET1)	$(TARGET2)						# Regla para compilar el programa
+all: $(TARGET1)	$(TARGET2) $(TARGET3)			# Regla para compilar el programa
 
 # Regla para compilar el creador
 $(TARGET1): $(SOURCE1)
@@ -15,6 +19,10 @@ $(TARGET1): $(SOURCE1)
 $(TARGET2): $(SOURCE2)
 	$(CC) $(CFLAGS) -o $(TARGET2) $(SOURCE2)
 
+# Regla para compilar estadistica
+$(TARGET3): $(SOURCE3)
+	$(CC) $(CFLAGS) -o $(TARGET3) $(SOURCE3)
+
 # Regla para ejecutar el creador
 cr: $(TARGET1)
 	./$(TARGET1)
@@ -23,6 +31,10 @@ cr: $(TARGET1)
 cl: $(TARGET2)
 	./$(TARGET2)
 
+# Regla para ejecutar las estadisticas
+est: $(TARGET3)
+	./$(TARGET3)
+
 # Regla para limpiar archivos basura
 clean:
-	rm -f $(TARGET1) $(TARGET2) creador cliente
+	rm -f $(TARGET1) $(TARGET2) $(TARGET2) creador cliente estadisticas
---
+++ estadisticas.c
@@ -0,0 +1,85 @@
+/*
+    Proceso encargado de mostrar las estadisticas asociadas a los procesos cliente y reconstructor
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>     // Incluir para la función sleep
+#include <semaphore.h>  // Para utilizar semaforos
+#include <fcntl.h>      // para la creacion del semaforo
+
+#define MEM_SIZE 20     // Tamaño de la memoria compartida en bytes
+
+sem_t semaforo;         // instancia de semaforo
+int *ptr_entero;        // puntero para la memoria compartida
+
+int main() {
+
+    /*--------------------ABRIR EL SEMAFORO CON EL NOMBRE----------------------*/
+    // Inicializa el semáforo para uso entre procesos
+    if (sem_init(&semaforo, 1, 1) == -1) {
+        perror("sem_init"); // Imprime el error si no se pudo inicializar
+        return 1;
+    }
+
+    /* ---------------- ACCEDER A MEMORIA COMPARTIDA ---------------------- */
+    
+    // Abre la memoria compartida
+    key_t clave = 1234; // Clave para la memoria compartida
+    // Acceder al segmento de memoria compartida
+    int id_memoria = shmget(clave, MEM_SIZE, 0666);
+    if (id_memoria == -1) {
+        perror("Error al acceder a la memoria compartida");
+        exit(1);
+    }
+
+    // Adjuntar el segmento de memoria compartida al espacio de direcciones del proceso
+    void *memoria = shmat(id_memoria, NULL, 0);
+    if (memoria == (void *) -1) {
+        perror("Error al adjuntar la memoria compartida");
+        exit(1);
+    }
+
+    ptr_entero = (int *)memoria;    // ver si debo poner el asterisco antes de ptr
+
+    while (1)
+    {
+        /* revisar que hayan terminado todos los procesos, si terminaron*/
+        if ((ptr_entero[1] == 1) && (ptr_entero[11] == 1)) // si ya terminaron el cliente y el reconstructor
+        {
+            sem_wait(&semaforo); // pedir semaforo
+            // mostrar las estadisticas segun los valores encontrados en memoria
+            printf("Estadísticas de los procesos. \n");
+            printf("Tiempo bloqueado del Cliente: %d", ptr_entero[2]);
+            printf("Tiempo bloquedado del Reconstructor: %d", ptr_entero[3]);
+            printf("Número de caracteres transferidos: %d", ptr_entero[0]);
+            printf("Número de caracteres en el buffer: %d", ptr_entero[4]);
+            printf("Espacio total de memoria utilizado: %d", ptr_entero[5]);
+            printf("Tiempo del Cliente en modo Usuario: %d", ptr_entero[6]);
+            printf("Tiempo del Cliente en modo Kernel: %d", ptr_entero[7]);
+            printf("Tiempo del Reconstructor en modo Usuario: %d", ptr_entero[8]);
+            printf("Tiempo del Reconstructor en modo Kernel: %d", ptr_entero[9]);
+            sem_post(&semaforo); // liberar semaforo
+        }
+        
+    }
+    
+    
+    // ptr_entero[contador] = caracter;                                // escribir en al direccion de memoria compartida
+
+
+
+    /*----------------------Desadjuntar la memoria compartida--------------------------*/
+    if (shmdt(memoria) == -1) {
+        perror("Error al desadjuntar la memoria compartida");
+        exit(1);
+    }
+
+    return 0;
+}
